40d21e42e69ac2a29d1970ce66d91ff57d3561d4
Billiefu <futk@mail2.sysu.edu.cn>
2025-08-04 02:46:08 +0800

    Update Project 02-02

--- projects/Project 02-02/02-02.py
+++ projects/Project 02-02/02-02.py
@@ -17,19 +17,29 @@ Full AGPLv3 text available in LICENSE file or at <https://www.gnu.org/licenses/a
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-image = cv.imread('../data/ctskull-256.bmp', cv.IMREAD_GRAYSCALE)
+# Load a grayscale image from the specified path
+image = cv.imread('../../images/ctskull-256.bmp', cv.IMREAD_GRAYSCALE)
+
+# Display the original image
 plt.axis('off')
 plt.imshow(image, cmap='gray')
 plt.title("Original Image")
 plt.show()
 
+# Initialize the current grayscale level. For an 8-bit image, it starts at 256 levels (0-255).
 grayscale = 256
+
+# Iterate 7 times to progressively reduce grayscale levels
 for k in range(7):
+    # Iterate through each pixel of the image
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
+            # Divide each pixel value by 2, effectively reducing the number of grayscale levels
             image[i, j] /= 2
 
+    # Update the displayed grayscale level count
     grayscale /= 2
+    # Display the image with reduced grayscale levels
     plt.axis('off')
     plt.imshow(image, cmap='gray')
     plt.title(f"Grayscale {grayscale} Image")
